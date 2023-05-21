96d5a21bfd97be0afd9cd647a5385634e5f30bed
Your Name <you@example.com>
2023-05-22 00:42:45 +0530

    helloooooo

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const ejs = require("ejs");
-const path = require("path");
 const bodyParser = require('body-parser');
 const app = express();
+const passport = require("passport");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+app.use(session({
+    secret:"TASK-USER",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+mongoose.set("strictQuery",false);
+mongoose.coonect("mongodb://127.0.0.1:27017/taskuserDB");
+
+const userSchema = mongoose.Schema({
+    email: String,
+    password: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = mongoose.model("User",userSchema);
 
 
 app.get("/",function(request,response){
